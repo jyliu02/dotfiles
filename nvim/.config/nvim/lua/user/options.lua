663e576758814b86e32665656b8fd4bf98132776
Aubrey Liu <aubreyliu233@gmail.com>
2024-06-29 12:15:00 +0800

    Auto backup: Sat Jun 29 12:15:00 CST 2024

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,33 +1,30 @@
+-- UI options
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.pumheight = 10
+vim.o.shortmess = "TIF"
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.wrap = false
+vim.o.incsearch = true
+vim.o.inccommand = "split"
+vim.o.scrolloff = 8
+vim.o.termguicolors = true
+vim.o.signcolumn = "yes:1"
+
+-- Editor options
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.clipboard = "unnamedplus"
-
-vim.o.splitbelow = true
-vim.o.splitright = true
-
-vim.o.inccommand = "split"
-
-vim.o.wrap = false
+vim.o.timeout = true
+vim.o.timeoutlen = 1500
+vim.o.updatetime = 50
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.smartindent = true
-vim.o.incsearch = true
-
 vim.o.showmode = false
-vim.o.termguicolors = true
-vim.o.scrolloff = 8
 vim.o.swapfile = false
-vim.o.backup = false
-vim.o.hidden = true
-vim.o.signcolumn = "yes:1"
-
-vim.o.timeout = true
-vim.o.updatetime = 50
-
-vim.o.pumheight = 10
-vim.o.shortmess = "TIF"
 vim.o.undofile = true
-vim.o.cursorline = true
+vim.o.backup = false
