67639de9ce2ee7c5cd51fb52fe59b25b31868ac2
Aubrey Liu <aubreyliu233@gmail.com>
2024-07-05 18:15:00 +0800

    Auto backup: Fri Jul  5 18:15:00 CST 2024

--- nvim/.config/nvim/lua/user/plugins/markdown.lua
+++ nvim/.config/nvim/lua/user/plugins/markdown.lua
@@ -5,7 +5,7 @@ return {
       { "<leader>g", "<cmd>GlobalNote<cr>", desc = "Toggle Global Note" },
     },
     opts = {
-      filename = "inbox.md",
+      filename = "index.md",
       directory = vim.fn.expand("~") .. "/Documents/Notes",
     },
   },
@@ -17,6 +17,36 @@ return {
     ft = { "markdown" },
     keys = {
       { "<leader>v", ft = "markdown", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
+    },
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    event = {
+      "BufReadPre " .. vim.fn.expand("~") .. "/Documents/Notes/**.md",
+      "BufNewFile " .. vim.fn.expand("~") .. "/Documents/Notes/**.md",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    init = function()
+      vim.o.conceallevel = 2
+    end,
+    opts = {
+      workspaces = {
+        {
+          name = "Notes",
+          path = "~/Documents/Notes",
+        },
+      },
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+      },
+      disable_frontmatter = true,
     },
   },
 }
