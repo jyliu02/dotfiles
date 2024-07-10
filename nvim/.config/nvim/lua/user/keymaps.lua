cddd4d5ac54c883228bcba9bf399c3e1f9e38d3a
Aubrey Liu <aubreyliu233@gmail.com>
2024-07-10 12:15:01 +0800

    Auto backup: Wed Jul 10 12:15:01 CST 2024

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -21,7 +21,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
 
-vim.keymap.set("x", "<leader>p", [[_dP"]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
