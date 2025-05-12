8154b06ffbcf64830128c5ab6f048be1667952cc
Junyu Liu <72335144+jyliu02@users.noreply.github.com>
2025-05-12 10:52:05 -0700

    Fix LSP configs

--- nvim/.config/nvim/lua/user/plugins/lsp.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp.lua
@@ -7,65 +7,56 @@ return {
       { "neovim/nvim-lspconfig" },
       { "williamboman/mason.nvim" },
       -- lsp progress
-      { "j-hui/fidget.nvim", opts = { progress = { display = { render_limit = 8 } } } },
+      { "j-hui/fidget.nvim",      opts = { progress = { display = { render_limit = 8 } } } },
     },
     config = function()
-      local on_attach = function(client, bufnr)
-        -- <C-w>d is the default mapping for viewing diagnostic under cursor
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0 })
-        vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = 0 })
-        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { buffer = 0 })
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          -- <C-w>d is the default mapping for viewing diagnostic under cursor
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0 })
+          vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = 0 })
+          vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { buffer = 0 })
 
-        if client.supports_method("textDocument/inlayHint") then
           vim.lsp.inlay_hint.enable(true, nil)
-        end
-      end
+        end,
+      })
+
+      vim.lsp.config('lua_ls', {
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            workspace = {
+              checkThirdParty = false,
+              library = vim.tbl_deep_extend("force", vim.api.nvim_get_runtime_file("", true), {
+                "${3rd}/luv/library",
+              }),
+            },
+            diagnostics = { disable = { "missing-fields" } },
+          },
+        }
+      })
+
+      vim.lsp.config('rust_analyzer',
+        {
+          settings = {
+            ["rust-analyzer"] = {
+              check = {
+                allTargets = false,
+              },
+              cargo = {
+                features = "all",
+              },
+            },
+          }
+        }
+      )
 
       require("mason").setup()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "rust_analyzer" },
-      })
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            on_attach = on_attach,
-          })
-        end,
-        ["lua_ls"] = function()
-          require("lspconfig").lua_ls.setup({
-            on_attach = on_attach,
-            settings = {
-              Lua = {
-                runtime = { version = "LuaJIT" },
-                workspace = {
-                  checkThirdParty = false,
-                  library = vim.tbl_deep_extend("force", vim.api.nvim_get_runtime_file("", true), {
-                    "${3rd}/luv/library",
-                  }),
-                },
-                diagnostics = { disable = { "missing-fields" } },
-              },
-            },
-          })
-        end,
-        ["rust_analyzer"] = function()
-          require("lspconfig").rust_analyzer.setup({
-            on_attach = on_attach,
-            settings = {
-              ["rust-analyzer"] = {
-                check = {
-                  allTargets = false,
-                },
-                cargo = {
-                  features = "all",
-                },
-              },
-            },
-          })
-        end,
       })
     end,
   },
@@ -90,11 +81,12 @@ return {
         python = { "autopep8" },
         cpp = { "clang-format" },
         html = { "prettier" },
+        rust = { "rustfmt", lsp_format = "fallback" },
       },
       default_format_opts = {
         timeout_ms = 3000,
-        async = false, -- not recommended to change
-        quiet = false, -- not recommended to change
+        async = false,           -- not recommended to change
+        quiet = false,           -- not recommended to change
         lsp_format = "fallback", -- not recommended to change
       },
     },
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -81,7 +81,7 @@ source $ZSH/oh-my-zsh.sh
 # export MANPATH="/usr/local/man:$MANPATH"
 
 # You may need to manually set your language environment
-# export LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
 
 # Preferred editor for local and remote sessions
 # if [[ -n $SSH_CONNECTION ]]; then
@@ -92,7 +92,7 @@ source $ZSH/oh-my-zsh.sh
 export EDITOR=nvim
 
 # Compilation flags
-# export ARCHFLAGS="-arch $(uname -m)"
+export ARCHFLAGS="-arch $(uname -m)"
 
 # Set personal aliases, overriding those provided by Oh My Zsh libs,
 # plugins, and themes. Aliases can be placed here, though Oh My Zsh
@@ -105,11 +105,12 @@ export EDITOR=nvim
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-alias vim=nvim
+
+eval "$(ssh-agent -s)" > /dev/null 2>&1
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519 > /dev/null 2>&1
 
 # Set up fzf key bindings and fuzzy completion
 source <(fzf --zsh)
-eval "$(zoxide init zsh)"
 
 # Set up the starship prompt
 eval "$(starship init zsh)"
